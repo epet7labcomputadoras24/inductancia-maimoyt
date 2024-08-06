c1103c916732c27039fa36d4cb876d294785e90a
maimoyt <maimoyt6@gmail.com>
2024-08-06 19:33:38 -0300

    tarea de maximo fuente

---
+++ tarea1/Program.vb
@@ -0,0 +1,16 @@
+Imports System
+Imports System.Security.Cryptography.X509Certificates
+
+Module Program
+    Sub Main(args As String())
+        Dim fr As Double
+        Dim ind As Double
+        Dim imp As Double
+        Console.WriteLine("escribir el valor de inductancia")
+        ind = Console.ReadLine()
+        Console.WriteLine("escribir el valor de frecuencia")
+        fr = Console.ReadLine()
+        imp = 2 * Math.PI * fr * ind
+        Console.WriteLine("la impedancia es en total" & imp)
+    End Sub
+End Module
